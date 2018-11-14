55fa47d7368746b9ba39a8ca090e26b2b62717d3
meteoricsnax <jesper.hingelbjerg@hotmail.com>
2018-11-14 11:21:41 +0100

    fixed jespers stuff

--- src/App.js
+++ src/App.js
@@ -3,25 +3,8 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-<<<<<<< HEAD
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React with CA3
-          </a>
-        </header>
-=======
+      <div>
         
->>>>>>> 8a04c0d775f23fdd65c3878d64f534ab0e9f3ce7
       </div>
     );
   }
