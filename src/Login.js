c9b7c505b93392ec30ade06c15575744a0150f8e
Albert Vandel <albertvandel@yahoo.dk>
2018-11-28 13:17:57 +0100

    added initial input validation and initial input feedback for login

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import { ShowError, ShowNeutral, ShowSuccess } from './ErrorMessage';
 
 class LogIn extends Component {
     constructor(props) {
       super(props);
-      this.state = { username: "", password: "" }
+      this.state = { username: "", password: "", clicked: false }
     }
     login = (evt) => {
       evt.preventDefault();
@@ -11,6 +12,18 @@ class LogIn extends Component {
     }
     onChange = (evt) => {
       this.setState({ [evt.target.id]: evt.target.value })
+      this.setState({clicked: false})
+    }
+    onClick =()=>{
+      this.setState({clicked: true})
+    }
+    feedBack(){
+      if (this.state.username != "" && this.state.clicked === true) {
+        return (<ShowSuccess message="Logged in" />)
+    }
+    if(this.state.username === "" && this.state.clicked === true) {
+        return (<ShowError message="Failed to Login"></ShowError>)
+    }
     }
     render() {
       return (
@@ -20,10 +33,11 @@ class LogIn extends Component {
               <input placeholder="User Name" id="username" className="form-control" />
               <input type="password" placeholder="Password" id="password" className="form-control"/>
               <div className="input-group-append">
-                <button className="btn btn-primary">Login</button>
+                <button onClick={this.onClick} className="btn btn-primary">Login</button>
               </div>
             </div>
           </form>
+          {this.feedBack()}
         </div>
       )
     }
